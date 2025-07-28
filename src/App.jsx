b22eeaf71278b5613348ef96b0ae2734dec32d72
warmboar <warmblackodor@gmail.com>
2025-07-28 12:51:10 +0300

    Force

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import DataTable from './components/DataTable';
+
+function App() {
+  return (
+    <div className="app-container">
+      <DataTable />
+    </div>
+  );
+}
+
+export default App;
